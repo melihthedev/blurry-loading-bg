fb42eaa9c1b5381abbac841469f7497d4ba3cc00
Melih Çetin <69591458+melihthedev@users.noreply.github.com>
2021-01-16 23:42:00 +0300

    Added main files

---
+++ script.js
@@ -0,0 +1,19 @@
+const bg = document.querySelector(".bg")
+const percent = document.querySelector(".loading-text")
+
+let load = 0;
+let int = setInterval(blurring, 15)
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
+
+function blurring() {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    percent.innerText = `${load}%`
+    percent.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
+}
